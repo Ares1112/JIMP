c169d1e423676ddaf1ae95125ccb29cae8c5e01d
Arkadiusz Blasiak <blasiak@mops.uci.agh.edu.pl>
2014-05-12 15:18:53 +0200

    Pierwszy commit

---
+++ aghMatrix.h
@@ -0,0 +1,20 @@
+#ifndef AGHMATRIX_H
+#define AGHMATRIX_H
+
+template <class T>
+class aghMatrix{
+    private:
+        int wier;
+        int kol;
+        T** tab;
+        T elem[]; 
+    public:
+        aghMatrix(){wier=0;kol=0;}
+        aghMatrix(int wier, int kol);
+        ~aghMatrix();
+        void setItem(int wier, int kol, T el);
+        void setItems(T* elem);
+        void setItems(int r, int c, ...);
+};
+
+#endif
